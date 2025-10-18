c313a0b527931b529770970681ce84f2a2971b51
Raj Kumar <imraaj1026@gmail.com>
2025-10-18 15:46:03 +0530

    lec-2  done

--- redis/day-1/index.js
+++ redis/day-1/index.js
@@ -1,18 +1,75 @@
-import express from 'express';
-import  {redis}  from './redis.js';
+// import express from 'express';
+// import  {redis}  from './redis.js';
 
-const app = express();
-const PORT = 4000;
+// const app = express();
+// const PORT = 4000;
 
-app.get('/',async(req,res)=>{
-    try {
-        const views = await redis.incr('pageViews')
-        res.send(`Number of views: ${views} times.`);
-    } catch (error) {
-        res.status(500).send('Error connecting to Redis');
-    }
-})
+// app.get('/',async(req,res)=>{
+//     try {
+//         const views = await redis.incr('pageViews')
+//         res.send(`Number of views: ${views} times.`);
+//     } catch (error) {
+//         res.status(500).send('Error connecting to Redis');
+//     }
+// })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+// app.listen(PORT,()=>{
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// })
+
+// day-2
+
+import Redis from 'ioredis';
+
+const redis = new Redis({
+    host:'localhost',
+    port:6379
+});
+
+async function main(){
+    // await redis.set('foo','bar');
+    // const value = await redis.get('foo');
+    // console.log(value);
+
+    // 1. Lists
+    // *. LPUSH
+    // await redis.lpush('mylist','a','b','c');
+
+    // *. RPUSH
+    // await redis.rpush('mylist','a','b','c');
+
+    // LPOP
+    // const leftPop = await redis.lpop('mylist');
+    // console.log('LPOP:', leftPop);
+
+    // RPOP
+    // const rightPop = await redis.rpop('mylist');
+    // console.log('RPOP:', rightPop);
+
+    // LLEN
+    // const listLength = await redis.llen('mylist');
+    // console.log('LLEN:', listLength);
+    
+    // // LRANGE
+    // const listItems = await redis.lrange('mylist',0,-1);
+    // console.log('LRANGE:', listItems);
+
+    // 2. Sets
+    // SADD
+    // const added=await redis.sadd('myset','a','b','c','a');
+    // console.log('SADD:', added);
+
+    // SREM 
+    // const removed=await redis.srem('myset','b');
+    // console.log('SREM:', removed);
+
+    // SISMEMBER
+    // const isMember=await redis.sismember('myset','a');
+    // console.log('SISMEMBER:', isMember);
+
+    // HASHMAP
+    await redis.hset('user:1000','name','Alice','age','30','city','New York');
+
+    await redis.hset('user:2000',{'name':'Bob','age':'25','city':'Los Angeles'});
+}
+main();
